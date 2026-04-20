2411afdda88a29e9ebdcd20548b66c6cf33a4da3
DaoLinh <daongoclinh1988@gmail.com>
2026-04-21 04:23:24 +0700

    second 21-Apr

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,3 +1,4 @@
+import  {Event}  from "@/database";
 import dbConnect from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -10,8 +11,11 @@ export async function POST(req: NextRequest) {
         try{
             event = Object.fromEntries(formData.entries())
         }catch (e){
-            return NextResponse.json({message:'Invalid Json data format'})
+            return NextResponse.json({message:'Invalid Json data format'}, {status: 400})
         }
+        
+        const createdEvent = await Event.create(event)
+        return NextResponse.json({message: 'Event created successfully', event: createdEvent }, {status: 201})
     }catch (e){
         console.log(e)
         return NextResponse.json({message: 'Event Creation failed', error: e instanceof Error? e.message :'unknown'})
