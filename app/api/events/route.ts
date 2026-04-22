963ab8ee30b2ca089e843b1bc6990cd1f33bee65
DaoLinh <daongoclinh1988@gmail.com>
2026-04-23 04:26:05 +0700

    23-Apr

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,8 +1,11 @@
 import  {Event}  from "@/database";
 import dbConnect from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
-export async function POST(req: NextRequest) {
+
+
+export async function POST(req:NextRequest) {
     try{
         await dbConnect()
         const formData = await req.formData()
@@ -13,7 +16,20 @@ export async function POST(req: NextRequest) {
         }catch (e){
             return NextResponse.json({message:'Invalid Json data format'}, {status: 400})
         }
-        
+        const file = formData.get('image') as File;
+
+        if(!file){
+            return NextResponse.json({message: 'Image file is required'}, {status: 400})
+        }
+        const arrayBuffer = await file.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+        const uploadResult = await new Promise((resolve, reject)=>{
+            cloudinary.uploader.upload_stream({resource_type:'image', folder:'DevEvent'}, (error, results)=>{
+                if(error) return reject(error)
+                resolve(results)
+            }).end(buffer)
+        })
+        event.image = (uploadResult as {secure_url:string}).secure_url
         const createdEvent = await Event.create(event)
         return NextResponse.json({message: 'Event created successfully', event: createdEvent }, {status: 201})
     }catch (e){
